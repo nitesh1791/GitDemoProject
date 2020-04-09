27719626b0d44a461de6bb4b32d131f63616a979
kshaik <kshaik@KShaik.csi.local>
2020-04-09 16:00:49 +0530

    Added one more check and commiting the file second time

--- GitDemoProject/src/Test2.java
+++ GitDemoProject/src/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is second class");
+		System.out.println("I have added one more check");
 
 	}
 
